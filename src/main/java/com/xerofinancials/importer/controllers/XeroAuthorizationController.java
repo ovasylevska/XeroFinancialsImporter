a91d0d20b96a2b4306c7b803a5c2b6a2151e2170
Olha Vasylevska <olha.vasylevska@gmail.com>
2020-04-21 16:40:36 +0300

    logging xero auth redirect

--- src/main/java/com/xerofinancials/importer/controllers/XeroAuthorizationController.java
+++ src/main/java/com/xerofinancials/importer/controllers/XeroAuthorizationController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 @Controller
@@ -50,7 +52,12 @@ public class XeroAuthorizationController {
 
     @GetMapping(value = {"/redirect/", "/redirect"})
     public String redirect(@RequestParam Map<String,String> requestParams, Model model) throws IOException {
-        logger.info("Processing Xero API redirect : " + requestParams);
+        final String params = requestParams.entrySet()
+                .stream()
+                .map(p -> urlEncodeUTF8(p.getKey()) + "=" + urlEncodeUTF8(p.getValue()))
+                .reduce((p1, p2) -> p1 + "&" + p2)
+                .orElse("");
+        logger.info("Processing Xero API redirect : " + params);
         callbackHandler.extractTokenInfo(requestParams);
         return "redirect:/xero/authorization";
     }
@@ -62,4 +69,11 @@ public class XeroAuthorizationController {
         return "redirect:/xero/authorization";
     }
 
+    static String urlEncodeUTF8(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new UnsupportedOperationException(e);
+        }
+    }
 }
